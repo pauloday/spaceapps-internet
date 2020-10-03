f43636eb009b542b2fae7b97c464dad12e1195ab
Dustin Morris <dustinnuc@gmail.com>
2020-10-03 14:48:26 -0700

    Added react-router Added about page Added home page Added simulation page

--- client/app.jsx
+++ client/app.jsx
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = React.lazy(() => import('./home.jsx'));
+const About = React.lazy(() => import('./about.jsx'));
+const Simulation = React.lazy(() => import('./simulation.jsx'));
 
 function App() {
-  // eslint-disable-next-line no-undef
+  // esli9n disable-next-line no-undef
   const viz = new Spacekit.Simulation(document.getElementById('sim'), {
     basePath: './',
     startDate: Date.now(),
   });
   console.log(viz);
   return (
-    <div>
-      howdy
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Internet On Mars!</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/simulation">Simulation</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/simulation">
+            <Simulation />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Home() {
+  return (
+    <p>Home Page</p>
+    <p>About Page</p>
+    <p>Simulation Page</p>
+  )
+}
