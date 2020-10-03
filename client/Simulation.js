a845ffbff3d22b32726746d6e8798f15bd1854a1
ertdfgcb <odaypaul@gmail.com>
2020-10-03 14:41:47 -0700

    add scaling to planets

--- client/Simulation.js
+++ client/Simulation.js
@@ -3,6 +3,14 @@
 const Simulation = new Spacekit.Simulation(document.getElementById('sim'), {
   basePath: './',
   startDate: Date.now(),
+  jdPerSecond: 25,
+  camera: {
+    initialPosition: [
+      -2.0014980565625981512,
+      2.030445338891231168,
+      0.73616394298897485,
+    ],
+  },
 });
 // Install scroll listener that stop animation while visualization is outside
 // of viewport.
@@ -13,6 +21,8 @@ Simulation.createSkybox(Spacekit.SkyboxPresets.NASA_TYCHO);
 
 // Create our first object - the sun - using a preset space object.
 Simulation.createObject('Sun', Spacekit.SpaceObjectPresets.SUN);
+Spacekit.SpaceObjectPresets.EARTH.particleSize = 100;
+Spacekit.SpaceObjectPresets.MARS.particleSize = 70;
 Simulation.createObject('Earth', Spacekit.SpaceObjectPresets.EARTH);
 Simulation.createObject('Mars', Spacekit.SpaceObjectPresets.MARS);
 
