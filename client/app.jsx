0fa36dd08461f89426f8e225e1c4033384e10439
ertdfgcb <odaypaul@gmail.com>
2020-10-03 11:30:22 -0700

    set up space kit

--- client/app.jsx
+++ client/app.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+  // eslint-disable-next-line no-undef
+  const viz = new Spacekit.Simulation(document.getElementById('sim'), {
+    basePath: './',
+    startDate: Date.now(),
+  });
+  console.log(viz);
   return (
     <div>
       howdy
