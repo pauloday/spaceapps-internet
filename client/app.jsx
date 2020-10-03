2c1efcbdf6726e20485c7e4e74c3317161986781
ertdfgcb <odaypaul@gmail.com>
2020-10-03 11:16:36 -0700

    initial commit

---
+++ client/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      howdy
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
