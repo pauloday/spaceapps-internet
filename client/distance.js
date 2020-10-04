7a0b3d26a7d2c3593ceb80200822807e3ab6cdfa
Dustin Morris <dustinnuc@gmail.com>
2020-10-04 12:31:48 -0700

    Math formulas for blocked signal

---
+++ client/distance.js
@@ -0,0 +1,29 @@
+function distance(x1,y1,x2,y2) {
+  return Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+};
+
+function distanceFromOrigin(x1,y1,x2,y2) {
+  const slope = (y2-y1)/(x2-x1);
+  let left = -1*slope*y1;
+  let right = -1*x1;
+  let b = slope;
+  let a = 1;
+  let c = -1*(left - right);
+  console.log(a,b,c);
+  let p = Math.abs(c);
+  p = p/Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+  return p;
+};
+
+const SUN_DIAMETER = 1392700000; //meters
+
+function blockedSignal(x1,y1,x2,y2) {
+  if(distanceFromOrigin(x1,y1,x2,y2) < SUN_DIAMETER/2) {
+    return true;
+  }
+  return false;
+}
+
+export distance;
+export distanceFromOrigin;
+export blockedSignal;
